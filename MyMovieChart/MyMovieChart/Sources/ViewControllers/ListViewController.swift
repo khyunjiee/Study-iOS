eb9849b2aed26f40cec0167224a18d33ab908e0c
khyunjiee <khyunjiee@gmail.com>
2019-12-14 23:18:45 +0900

    [UPDATE] original MyMovieChart + API

--- MyMovieChart/MyMovieChart/Sources/ViewControllers/ListViewController.swift
+++ MyMovieChart/MyMovieChart/Sources/ViewControllers/ListViewController.swift
@@ -10,29 +10,18 @@ import UIKit
 
 class ListViewController: UITableViewController {
     
-    var dataset = [
-        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight"),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain"),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret")
-    ]
-    
     // 테이블 뷰를 구성할 리스트 데이터
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
-        for (title, desc, opendate, rating, thumbnail) in self.dataset {
-            let mvo = MovieVO()
-            mvo.title = title
-            mvo.description = desc
-            mvo.opendate = opendate
-            mvo.rating = rating
-            mvo.thumbnail = thumbnail
-            
-            datalist.append(mvo)
-        }
         return datalist
     }()
-
+    
+    var page = 1
+    
+    @IBOutlet var moreBtn: UIButton!
+    
     override func viewDidLoad() {
+        self.callMovieAPI()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -51,12 +40,77 @@ class ListViewController: UITableViewController {
         cell.desc?.text = row.description
         cell.opendate?.text = row.opendate
         cell.rating?.text = "\(row.rating!)"
-        cell.thumbnail.image = UIImage(named: row.thumbnail!)
+        
+        // 섬네일 경로를 인자값으로 하는 URL 객체를 생성
+        let url: URL! = URL(string: row.thumbnail!)
+        // 이미지를 읽어와 Data 객체에 저장
+        let imageData = try! Data(contentsOf: url)
+        // UIImage 객체를 생성하여 아울렛 변수의 image 속성에 대입
+        cell.thumbnail.image = UIImage(data: imageData)
         
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NSLog("선택된 행은 \(indexPath.row)번쨰 행입니다")
+    }
+    
+    @IBAction func more(_ sender: Any) {
+        // 현재 페이지 값에 1을 추가한다.
+        self.page += 1
+        // 영화차트 API를 호출한다.
+        self.callMovieAPI()
+        // 데이터를 다시 읽어오도록 테이블 뷰를 갱신한다.
+        self.tableView.reloadData()
+    }
+    
+    func callMovieAPI() {
+        // 호핀 API 호출을 위한 URI를 생성
+        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=\(self.page)&count=30&genreId=&order=releasedateasc"
+        let apiURI: URL! = URL(string: url)
+        
+        //REST API를 호출
+        let apidata = try! Data(contentsOf: apiURI)
+        
+        // 데이터 전송 결과를 로그로 출력
+        let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? "데이터가 없습니다"
+        NSLog("API Result = \(log)")
+        
+        // JSON 객체를 파싱하여 NSDictionary 객체로 변환
+        do {
+            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+            
+            // 데이터 구조에 따라 차례대로 캐스팅하며 읽어온다.
+            let hoppin = apiDictionary["hoppin"] as! NSDictionary
+            let movies = hoppin["movies"] as! NSDictionary
+            let movie = movies["movie"] as! NSArray
+            
+            // Iterator 처리를 하면서 API 데이터를 MovieVO 객체에 저장한다.
+            for row in movie {
+                // 순회 상수를 NSDictionary 타입으로 캐스팅
+                let r = row as! NSDictionary
+                // 테이블 뷰 리스트를 구성할 데이터 형식
+                let mvo = MovieVO()
+                
+                // movie 배열의 각 데이터를 mvo 상수의 속성에 대입
+                mvo.title = r["title"] as? String
+                mvo.description = r["genreNames"] as? String
+                mvo.thumbnail = r["thumbnailImage"] as? String
+                mvo.detail = r["linkUrl"] as? String
+                mvo.rating = ((r["ratingAverage"] as! NSString).doubleValue)
+                
+                self.list.append(mvo)
+                
+                // 전체 데이터 카운트를 얻는다.
+                let totalCount = (hoppin["totalCount"] as? NSString)!.integerValue
+                
+                // totalCount가 읽어온 데이터 크기와 같거나 클 경우 더보기 버튼을 막는다.
+                if(self.list.count >= totalCount){
+                    self.moreBtn.isHidden = true
+                }
+            }
+        } catch {
+            NSLog("Parse Error!!")
+        }
     }
 }
--- README.md
+++ README.md
@@ -5,6 +5,9 @@
 * use TableView (Basic, Subtitle, Custom cell)
 
   <img src="./images/MyMovieChart_Subtitle.png" width="30%" height="30%"></img>
-
   <img src="./images/MyMovieChart_Custom.png" width="30%" height="30%"></img>
 
+* use API
+
+  <img src="./images/MyMovieChart_API1.png" width="30%" height="30%"></img>
+  <img src="./images/MyMovieChart_API2.png" width="30%" height="30%"></img>
