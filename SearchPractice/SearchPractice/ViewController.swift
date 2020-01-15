c66c60ee8aac579e0c86befc0ad2b4aba9755964
khyunjiee <khyunjiee@gmail.com>
2020-01-15 19:08:29 +0900

    [Upload] iOS 프로젝트 생성

---
+++ SearchPractice/SearchPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchPractice
+//
+//  Created by 김현지 on 2019/12/27.
+//  Copyright © 2019 김현지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
