747a448ea8306b1dcfb290357bdee7ca3f1e0ed5
khyunjiee <khyunjiee@gmail.com>
2020-03-28 03:49:43 +0900

    [Upload] 텍스트 필드 델리게이션 공부 프로젝트

---
+++ Delegate-TextField/Delegate-TextField/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  Delegate-TextField
+//
+//  Created by 김현지 on 2020/03/28.
+//  Copyright © 2020 김현지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var tf: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 텍스트 필드 속성 설정
+        self.tf.placeholder = "값을 입력하세요."
+        self.tf.keyboardType = UIKeyboardType.alphabet  // 키보드 타입 영문자 패드로
+        self.tf.keyboardAppearance = UIKeyboardAppearance.dark  // 키보드 스타일 어둡게
+        self.tf.returnKeyType = UIReturnKeyType.join
+        self.tf.enablesReturnKeyAutomatically = true    // 리턴키 자동 활성화 On
+        
+        // 텍스트 필드 스타일 설정
+        self.tf.borderStyle = UITextField.BorderStyle.line
+        self.tf.backgroundColor = UIColor(white: 0.87, alpha: 1.0)
+        self.tf.contentVerticalAlignment = .center  // 수직 방향으로 텍스트가 가운데 정렬되도록
+        self.tf.contentHorizontalAlignment = .center    // 수평 방향으로 텍스트가 가운데 정렬되도록
+        self.tf.layer.borderColor = UIColor.darkGray.cgColor    // 테두리 색상을 회색으로
+        self.tf.layer.borderWidth = 2.0
+        
+        // 텍스트 필드를 최초 응답자로 지정
+        self.tf.becomeFirstResponder()
+        
+        self.tf.delegate = self
+        
+    }
+
+    @IBAction func confirm(_ sender: Any) {
+        // 텍스트 필드를 최초 응답자 객체에서 삭제
+        self.tf.resignFirstResponder()
+    }
+    
+    @IBAction func input(_ sender: Any) {
+        // 텍스트 필드를 최초 응답자 객체로 지정
+        self.tf.becomeFirstResponder()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("텍스트 필드의 편집이 시작되었습니다.")
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 내용이 삭제됩니다.")
+        return true // false를 리턴하면 삭제되지 않는다.
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print("텍스트 필드의 내용이 \(string)으로 변경됩니다.")
+        if Int(string) == nil { // 입력된 값이 숫자가 아니라면 true를 리턴
+            // 현재 텍스트 필드에 입력된 길이와 더해질 문자열 길이의 합이 10을 넘는다면 반영하지 않음
+            if (textField.text?.count)! + string.count > 10 {
+                return true
+            } else {
+                return false
+            }
+        } else {    // 입력된 값이 숫자라면 false를 리턴
+            return false
+        }
+    }
+    
+    // 텍스트 필드의 리턴키가 눌려졌을 때 호출
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 리턴키가 눌려졌습니다.")
+        return true
+    }
+    
+    // 텍스트 필드의 편집이 종료될 때 호출
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 편집이 종료됩니다.")
+        return true // false를 리턴하면 편집이 종료되지 않는다.
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        print("텍스트 필드의 편집이 종료되었습니다.")
+    }
+}
+
